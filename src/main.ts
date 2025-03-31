49acf05921b8b4b8f855bdb0ef86dbdd3e4fc7c6
wenyiqing <wenyiqing@tencent.com>
2025-03-31 20:50:51 +0800

    引入tdesign样式

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia';
 import { Button, Dialog, Input, Alert } from 'tdesign-vue-next';
 import router from './router/index';
 import i18n from './locales';
+import 'tdesign-vue-next/es/style/index.css';
 
 const app = createApp(App);
 app.use(router);
